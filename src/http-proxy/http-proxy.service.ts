d6d3e37349d199a216f8d02fb144512c3da9d665
mohsennrs <mohsen.nurisa@gmail.com>
2023-12-01 11:53:52 +0000

    logging added to http-proxy

--- src/http-proxy/http-proxy.service.ts
+++ src/http-proxy/http-proxy.service.ts
@@ -3,13 +3,15 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 export default class HttpProxyService {
   public httpProxy;
   constructor(targetUrl: string | undefined) {
-    if (targetUrl === undefined) return;
+    if (targetUrl === undefined || !targetUrl) return;
     this.httpProxy = createProxyMiddleware({
       target: targetUrl ?? 'http://www.example.org',
       secure: false,
       pathRewrite: {
         '^/api/v1': '', // Remove the initial "/api/v1" from the URL
       },
+      changeOrigin: true,
+      logLevel: 'debug',
       //   onProxyReq: (Req, req, res) => {
       //     // console.log(proxyReq);
       //     console.log(
